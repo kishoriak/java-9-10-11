c14a6f6020d0632613567c464f5b129582b23468
kishoriak <60075030+kishoriak@users.noreply.github.com>
2021-04-22 11:24:59 +0530

    Add files via upload

---
+++ testdoc123/Tester.java
@@ -0,0 +1,19 @@
+
+/**
+  *@author  xxxx
+  *@version 0.1
+
+*/
+
+
+public class Tester{
+/**
+  *Main method to run
+  *<p>Hello Demo</p>
+  *@param args  commandline argument
+*/
+
+public static void main(String[] args){
+
+  System.out.println("Hello from tester");
+}}
